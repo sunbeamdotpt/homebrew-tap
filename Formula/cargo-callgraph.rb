af16e310cce1462a83a8dddd73d97536e625a296
Sienna Meridian Satterwhite <sienna@sunbeam.pt>
2026-07-20 15:36:44 +0100

    cargo-callgraph 0.3.1

--- Formula/cargo-callgraph.rb
+++ Formula/cargo-callgraph.rb
@@ -1,8 +1,8 @@
 class CargoCallgraph < Formula
   desc "Generate callgraphs for Rust workspaces using rust-analyzer"
   homepage "https://github.com/sunbeamdotpt/cargo-callgraph"
-  url "https://github.com/sunbeamdotpt/cargo-callgraph/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "e85cea06ef7fa892d094a60646690d7417e93216f0a127ce63c65f60a306ea7f"
+  url "https://github.com/sunbeamdotpt/cargo-callgraph/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "77eb83bdd10c791fa158ef85e2aa5be2bd56ae2eab6d00e21051962505799037"
   license "AGPL-3.0-or-later"
   head "https://github.com/sunbeamdotpt/cargo-callgraph.git", branch: "mainline"
 
